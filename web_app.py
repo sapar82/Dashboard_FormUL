bbec06a4238df6cc5593e461ce32a0d0f11b958c
Sammy NP <sapar82#@ulaval.ca>
2023-01-18 15:02:25 -0500

    Change plotting, now with bokeh

--- web_app.py
+++ web_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+from bokeh.plotting import figure
 from test import test_decode_file
 df = pd.read_csv('decoded_data_7_aout.csv')
 #df.set_index('time', inplace=True)
@@ -9,9 +10,9 @@ st.title('FormUL Dashboard')
 
 left_column, right_column = st.columns(2)
 
-log = st.file_uploader("Upload a log_file", type="txt", key=6)
+log = st.file_uploader("Upload a log_file", type="txt", key=1)
 
-dbc_file = st.file_uploader("Upload a dbc_file", type="dbc", key=7)
+dbc_file = st.file_uploader("Upload a dbc_file", type="dbc", key=2)
 if dbc_file is None:
     dbc_file = 'dbc/formul_16_jan.dbc'
 
@@ -32,27 +33,32 @@ else:
 with left_column:
     option_left = st.multiselect(
         'Choose a variable',
-        df.columns.to_list()[1:], key=15)
+        df.columns.to_list()[1:], key=3,
+        max_selections=5)
     second_option_left = st.selectbox(
         'Choose a way of showing data',
-        ['Line Chart', "Dataframe"], key=3)
+        ['Line Chart', "Dataframe"], key=4)
 
     if option_left == []:
         option_left = [df.columns.to_list()[1]]
 
     if second_option_left == 'Line Chart':
-        if type(option_left) == list and len(option_left) > 1:
-            fig, ax = plt.subplots()
+        
+        p = figure(
+                    title='Data',
+                    x_axis_label='time',
+                    y_axis_label='y')
+        possible_colors = ['red', 'blue', 'green', 'yellow', 'black']
+        for n, i in enumerate(option_left):
+            graph_data = df[['time', i]].dropna()
 
-            for i in option_left:
-                graph_data = df[i].dropna()
-                ax.plot(graph_data, label=i)
-            ax.legend()
-            st.pyplot(fig)
-        else:
-            graph_data = df[option_left].dropna()
-            st.subheader(option_left)
-            st.line_chart(graph_data,y=option_left)
+            #p.line(graph_data['time'], graph_data[i], legend_label=i, line_width=2)
+            #Change color
+            p.line(graph_data['time'], graph_data[i], legend_label=i, line_width=2, color=possible_colors[n])
+        st.bokeh_chart(p, use_container_width=True)
+
+        graph_data = df[['time', option_left[0]]].dropna()
+        st.subheader(option_left)
     else:
         if type(option_left) == list and len(option_left) > 1:
             st.subheader(option_left)
@@ -62,19 +68,38 @@ with left_column:
             st.write("TODO")
 
 with right_column:
-
-    option_right = st.selectbox(
+    option_right = st.multiselect(
         'Choose a variable',
-        df.columns.to_list()[1:], key=2)
-
+        df.columns.to_list()[1:], key=6,
+        max_selections=5)
     second_option_right = st.selectbox(
         'Choose a way of showing data',
-        ['Line Chart', "Dataframe"], key=4)
-    
+        ['Line Chart', "Dataframe"], key=7)
+
+    if option_right == []:
+        option_right = [df.columns.to_list()[2]]
+
     if second_option_right == 'Line Chart':
-        graph_data = df[option_right].dropna()
+        
+        p_right = figure(
+                    title='Data',
+                    x_axis_label='time',
+                    y_axis_label='y')
+        possible_colors = possible_colors[::-1]
+        for n, i in enumerate(option_right):
+            graph_data = df[['time', i]].dropna()
+
+            #p.line(graph_data['time'], graph_data[i], legend_label=i, line_width=2)
+            #Change color
+            p_right.line(graph_data['time'], graph_data[i], legend_label=i, line_width=2, color=possible_colors[n])
+        st.bokeh_chart(p_right, use_container_width=True)
+
+        graph_data = df[['time', option_right[0]]].dropna()
         st.subheader(option_right)
-        st.line_chart(graph_data, y=option_right)
     else:
-        st.subheader(option_right)
-        st.dataframe(df[option_right].dropna())
+        if type(option_right) == list and len(option_right) > 1:
+            st.subheader(option_right)
+            st.dataframe(df[option_right].dropna())
+        else:
+            data = pd.DataFrame()
+            st.write("TODO")
